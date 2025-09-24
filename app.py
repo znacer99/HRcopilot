863b60b32a5f5bb4e971dd20c9fba23dc2be2011
znacer <naceurez95@gmail.com>
2025-09-24 10:43:02 +0200

    logo aaa

--- app.py
+++ app.py
@@ -227,6 +227,11 @@ def index():
         logout_user()
     return redirect(url_for('auth.login'))
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
